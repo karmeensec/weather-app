4f03f6cb6402c3729c4db05ea7dc8dda54fd7f7a
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-25 23:15:50 +0100

    Basic components added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ function App() {
 
   return (
 
-    <div></div>
+    <main className='bg-gradient-to-b from-gray-700 via-gray-900 to-black flex justify-center items-center w-full h-[100vh]'>
+        
+        <section className='bg-cyan-500 w-full h-full flex items-center justify-center flex-col md:max-w-[600px] lg:h-[600px]'>
+            
+            <h1>Weather Forecast</h1>
+
+            <p>Enter your location below</p>
+
+        </section>
+
+    </main>
 
   )
 }
