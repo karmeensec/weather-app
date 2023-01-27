2936f3f38cf2c350f5dd75a5d4ca49792a43329a
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-28 00:47:04 +0100

    Refactoring the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,12 @@
-import { ChangeEvent, useEffect, useState } from 'react'
 import './App.css'
-import { ForecastTtypes, OptionTypes } from './types'
+import ForecastHooks from './hooks/ForecastHooks';
 import Weather from './Weather'
 
-const limit = 5; 
+
 
 function App(): JSX.Element {
 
-  const [name, setName] = useState<string>('');
-  const [options, setOptions] = useState<[]>([]);
-  const [cityData, setCityData] = useState<OptionTypes | null>(null);
-  const [forecast, setForecast] = useState<ForecastTtypes | null>(null);
-
-
-
-   const getSearchNames =  function(value: string) {
-
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=${limit}&appid=${import.meta.env.VITE_REACT_API_KEY}`).then(res=> res.json()).then((data)=> setOptions(data));
-
-  }
-
-
-  const InputFieldChange = function(e: ChangeEvent<HTMLInputElement>) {
-
-    const value = e.target.value.trim();
-
-    setName(value);
-
-    if (value === '') return;
-
-    getSearchNames(value);
-
-  }
-
-  const OptionFieldSelect = function(opt: OptionTypes) {
-
-    setCityData(opt);
-
-  }
-
-  const getWeatherData = (opt: OptionTypes) => {
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${opt.lat}&lon=${opt.lon}&units=metric&appid=${import.meta.env.VITE_REACT_API_KEY}`).then(res => res.json()).then((data)=> console.log(data));
-
-  }
-  
-
-  const SubmitCity = function() {
-
-    if (!cityData) return;
-
-    getWeatherData(cityData);
-
-  }
-
-
-
-
-  useEffect( ()=> {
-
-    if (cityData) {
-
-      setName(cityData.name);
-      setOptions([]);
-
-    }
-
-  },  [cityData]);
-
+  const {name, options, forecast, InputFieldChange, SubmitCity, OptionFieldSelect} = ForecastHooks();
 
 
   return (
