f120db5e57e58f74a65cf860628606e721d27cdb
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-28 01:14:42 +0100

    Forecast types added

--- src/types.tsx
+++ src/types.tsx
@@ -5,3 +5,20 @@ export type OptionTypes = {
     lon: number,
 
 }
+
+export type ForecastTtypes = {
+
+    name: string,
+    country: string,
+    sunrise: number,
+    sunset: number,
+
+    list: [{ dt: number,
+         clouds: {all: number},
+          main: { feels_like: number, humidity: number, pressure: number, temp: number, temp_max: number, temp_min: number },
+           weather: [{description: string, icon: string, main: string}],
+            wind: {speed: number, deg: number, gust: number},
+             pop: number,
+              visibility: number }]
+
+}
